d5ac44c21adfac20d01118c9bc98854aeeea45b9
SaraswatiKumari1 <kri.saras123@gmail.com>
2026-02-14 17:45:22 +0530

    Implemented useState for filtering Toprated restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,25 @@
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import {useState} from "react";
 
 const Body = () => {
+
+    const arr = useState(resList);
+    const listOfRestaurant = arr[0];
+    const setFilteredResList = arr[1];
+  //const [listOfRestaurant, setFilteredResList] = useState(resList);
   return (
     <div className="body">
-      <div className="search-bar">Search for your favorite meals here...</div>
+      <div className="filter">
+        <button className="filter-btn" onClick={() => {
+            const filteredList = listOfRestaurant.filter((res) => res.info.rating?.aggregate_rating > 4);
+            setFilteredResList(filteredList);
+        }}>
+            Top Rated Restaurants</button>
+        </div>
       <div className="restaurant-container">
-        {console.log(resList)}
-        {resList.map((restaurant) => {
+        {/*{console.log(listOfRestaurant)} */}
+        {listOfRestaurant.map((restaurant) => {
             return <RestaurantCard
               key={restaurant.info.resId}
               resData={restaurant}
